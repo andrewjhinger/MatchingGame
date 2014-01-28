2029d3357c8047eef154db08cc53bda45eb0a881
andrewjhinger <andrewjhinger@gmail.com>
2014-01-28 16:17:55 -0500

    j

---
+++ MatchingGame/MatchingGame.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MatchingGame
+{
+    partial class MatchingGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.x2GridToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x4GridToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x6GridToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(292, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 24);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(292, 249);
+            this.mainPanel.TabIndex = 1;
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.x2GridToolStripMenuItem,
+            this.x4GridToolStripMenuItem,
+            this.x6GridToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.resetToolStripMenuItem.Text = "&Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // x2GridToolStripMenuItem
+            // 
+            this.x2GridToolStripMenuItem.Name = "x2GridToolStripMenuItem";
+            this.x2GridToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.x2GridToolStripMenuItem.Text = "2x2 Grid";
+            this.x2GridToolStripMenuItem.Click += new System.EventHandler(this.x2GridToolStripMenuItem_Click);
+            // 
+            // x4GridToolStripMenuItem
+            // 
+            this.x4GridToolStripMenuItem.Name = "x4GridToolStripMenuItem";
+            this.x4GridToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.x4GridToolStripMenuItem.Text = "4x4 Grid";
+            this.x4GridToolStripMenuItem.Click += new System.EventHandler(this.x4GridToolStripMenuItem_Click);
+            // 
+            // x6GridToolStripMenuItem
+            // 
+            this.x6GridToolStripMenuItem.Name = "x6GridToolStripMenuItem";
+            this.x6GridToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.x6GridToolStripMenuItem.Text = "6x6 Grid";
+            this.x6GridToolStripMenuItem.Click += new System.EventHandler(this.x6GridToolStripMenuItem_Click);
+            // 
+            // MatchingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MatchingGame";
+            this.Text = "MatchingGame";
+            //   this.Layout += new System.Windows.Forms.LayoutEventHandler(this.MatchingGame_Layout);
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem x2GridToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x4GridToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x6GridToolStripMenuItem;
+    }
+}
+
---
+++ MatchingGame/MatchingGame.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class MatchingGame : Form
+    {
+        private Button[,] _buttons;         // Two-dimensional array of Button controls
+        private int _buttonRows = 4;        // Number of rows (and columns) of Button controls
+
+        private Button _firstButton;        // First selected Button
+        private bool _picking = false;      // Flag to indicate if we've selected first Button
+
+        public MatchingGame()
+        {
+            // Create Button controls
+            createButtons();
+
+            // Now initialize components
+            InitializeComponent();
+
+            MatchingGame_Layout();
+
+            // Set the card letters
+            setCards();
+        }
+
+        private void createButtons()
+        {
+            // Dimension two-dimensional array
+            _buttons = new Button[_buttonRows, _buttonRows];
+
+            // Loop through each row and column of buttons
+            for (int row = 0; row < _buttonRows; row++)
+            {
+                for (int col = 0; col < _buttonRows; col++)
+                {
+                    // Create the new Button, setting Button properties
+                    Button button = new Button()
+                    {
+                        Text = "?",
+                        Font = new Font(FontFamily.GenericSansSerif, 20, FontStyle.Bold),
+                        ForeColor = Color.Black
+                    };
+                    // Set the current array element to reference the new Button
+                    _buttons[row, col] = button;
+
+                    // Add an event handler for this Button (same one for all of the Button controls)
+                    _buttons[row, col].Click += new EventHandler(buttonClickHandler);
+                }
+            }
+        }
+
+        private void buttonClickHandler(object sender, EventArgs e)
+        {
+            // Get the Button control we're responding to
+            Button button = (Button)sender;
+
+            // Make sure we aren't clicking on the same button both times
+            // or we're selecting a button that's already part of a pair
+            if (button.Text == "?")
+            {
+                if (!_picking)
+                {
+                    // First pick
+                    _firstButton = button;
+                    button.Text = button.Tag.ToString();
+                    button.ForeColor = Color.Red;
+                }
+                else if (_firstButton.Tag.ToString() == button.Tag.ToString())
+                {
+                    // Matched
+                    button.Text = button.Tag.ToString();
+                    button.ForeColor = Color.Green;
+                    _firstButton.ForeColor = Color.Green;
+                }
+                else
+                {
+                    // Second pick failed to match
+                    button.Text = button.Tag.ToString();
+                    button.ForeColor = Color.Red;
+
+                    // We need to refresh the Button before sleeping or the Button
+                    // text change will not be visible
+                    button.Refresh();
+
+                    // Pause for player to see the selected Button text
+                    System.Threading.Thread.Sleep(1000);
+
+                    // Change Button controls back to default text and colors
+                    _firstButton.Text = "?";
+                    button.Text = "?";
+                    button.ForeColor = Color.Black;
+                    _firstButton.ForeColor = Color.Black;
+                }
+
+                // Flip our picking flag
+                _picking = !_picking;
+            }
+        }
+
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Exit the application
+            this.Close();
+        }
+
+        private void MatchingGame_Layout()
+        {
+            // Get the height and width for each Button using the inside height and
+            // width of the Panel control
+
+            mainPanel.Controls.Clear();
+
+            int buttonWidth = mainPanel.ClientRectangle.Width / _buttonRows;
+            int buttonHeight = mainPanel.ClientRectangle.Height / _buttonRows;
+
+            // Loop through and set the location and dimensions of each Button control
+            for (int row = 0; row < _buttonRows; row++)
+            {
+                for (int col = 0; col < _buttonRows; col++)
+                {
+                    _buttons[row, col].SetBounds(buttonWidth * col, buttonHeight * row, buttonWidth, buttonHeight);
+                }
+            }
+
+            // Add the Button controls to the Panel Controls collection
+            if (mainPanel.Controls.Count == 0)
+            {
+                for (int row = 0; row < _buttonRows; row++)
+                {
+                    for (int col = 0; col < _buttonRows; col++)
+                    {
+                        mainPanel.Controls.Add(_buttons[row, col]);
+                    }
+                }
+            }
+        }
+
+        private void setCards()
+        {
+            string _alphabet = "ABCDEFDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDGHABCDEFGH";
+            Random random = new Random();
+
+            // Loop through each Button
+            for (int row = 0; row < _buttonRows; row++)
+            {
+                for (int col = 0; col < _buttonRows; col++)
+                {
+                    // Find a random number into card text string
+                    int position = random.Next(_alphabet.Length);
+
+                    // Extract the card text
+                    string cardText = _alphabet[position].ToString();
+
+                    // Store the card text in the Tag property of our Button control
+                    ((Button)_buttons[row, col]).Tag = cardText;
+
+                    // Use a temporary string to create a new card text string excluding
+                    // the previously used card text
+                    string holdString = "";
+                    if (position > 0)
+                        holdString = _alphabet.Substring(0, position);
+                    if (position < _alphabet.Length)
+                        holdString += _alphabet.Substring(position + 1, _alphabet.Length - position - 1);
+
+                    // Replace previous card text string with the temporary card text string
+                    _alphabet = holdString;
+                }
+            }
+        }
+
+        private void restartGame()
+        {
+            createButtons();
+            MatchingGame_Layout();
+            setCards();
+            this.PerformLayout();
+            this.Refresh();
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            restartGame();
+        }
+
+        private void x2GridToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _buttonRows = 2;
+            restartGame();
+        }
+
+        private void x4GridToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _buttonRows = 4;
+            restartGame();
+        }
+
+        private void x6GridToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _buttonRows = 6;
+            restartGame();
+        }
+    }
+}
